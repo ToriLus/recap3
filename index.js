77f1c25d1e454278f130c19b93d20a830a13107a
= <=>
2023-04-06 17:16:27 +0200

    Simplyfy nexbutton event listeners

--- index.js
+++ index.js
@@ -20,57 +20,14 @@ let fetchurl = `https://rickandmortyapi.com/api/character?page=${page}`;
 let maxPage;
 console.log(maxPage);
 
-await fetchCharacter(fetchurl);
-
-export async function fetchCharacter(url) {
-  try {
-    cardContainer.innerHTML = "";
-    const character = await fetch(url);
-    const fetchedCharacterData = await character.json();
-    const fetchedCharacter = fetchedCharacterData.results;
-    console.log("results", fetchedCharacter);
-    if (fetchedCharacter) {
-      fetchedCharacter.forEach((character) => {
-        cardContainer.append(
-          createCharacterCard(
-            character.name,
-            character.status,
-            character.type,
-            character.episode.length,
-            character.image
-          )
-        );
-      });
-    }
-    console.log("maxPage", maxPage);
-    maxPage = fetchedCharacterData.info.pages;
-    createPagination(maxPage, page);
-  } catch (error) {
-    maxPage = 1;
-    page = 1;
-    createPagination(maxPage, page);
-    // alert("go cry in a corner");
-  }
-}
+await fetchCharacters(fetchurl);
 
 nextButton.addEventListener("click", async () => {
-  cardContainer.innerHTML = "";
-  page = page + 1;
-  if (page >= maxPage) {
-    page = maxPage;
-  }
-  fetchurl = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`;
-  await fetchCharacter(fetchurl);
-  console.log(maxPage);
+  changePage(1);
 });
+
 prevButton.addEventListener("click", async () => {
-  cardContainer.innerHTML = "";
-  page = page - 1;
-  if (page <= 0) {
-    page = 1;
-  }
-  fetchurl = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`;
-  await fetchCharacter(fetchurl);
+  changePage(-1);
 });
 
 searchBarContainer.append(createSearchBar());
@@ -85,8 +42,55 @@ searchBar.addEventListener("submit", async (e) => {
   fetchurl = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`;
   try {
     page = 1;
-    await fetchCharacter(fetchurl);
+    await fetchCharacters(fetchurl);
   } catch (error) {
     console.log("nothing");
   }
 });
+
+export function setPage(value) {
+  page = value;
+}
+export function getPage() {
+  return page;
+}
+
+export async function fetchCharacters(url) {
+  try {
+    cardContainer.innerHTML = "";
+    const character = await fetch(url);
+    const fetchedCharactersData = await character.json();
+    const fetchedCharacters = fetchedCharactersData.results;
+    console.log("results", fetchedCharacters);
+    if (fetchedCharacters) {
+      fetchedCharacters.forEach((character) => {
+        cardContainer.append(
+          createCharacterCard(
+            character.name,
+            character.status,
+            character.type,
+            character.episode.length,
+            character.image
+          )
+        );
+      });
+    }
+    console.log("maxPage", maxPage);
+    maxPage = fetchedCharactersData.info.pages;
+    createPagination(maxPage, page);
+  } catch (error) {
+    maxPage = 1;
+    page = 1;
+    createPagination(maxPage, page);
+  }
+}
+
+// Change page
+async function changePage(changeDirection) {
+  changeDirection > 0 ? page++ : page--;
+  if (page <= 0) page = 1;
+  if (page >= maxPage) page = maxPage;
+  fetchurl = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`;
+  await fetchCharacters(fetchurl);
+  console.log(maxPage);
+}
