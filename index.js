40e62aa6bcf0191e62832d67f1aa0e995628b348
Wiktoria Luszczek <tori@LAPTOP-9BFLS748>
2023-04-06 12:06:38 +0200

    added the url as an argument into the fetchcharacter function

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+export const maxPage = 1;
+export const page = 1;
+export const searchQuery = "";
+export const fetchurl = `https://rickandmortyapi.com/api/character?page=${page}`;
 
-async function fetchCharacter() {
+export async function fetchCharacter(url) {
   try {
-    let character = await fetch("https://rickandmortyapi.com/api/character");
+    let character = await fetch(url);
     let fetchedCharacter = await character.json();
     fetchedCharacter = fetchedCharacter.results;
 
@@ -37,4 +38,4 @@ async function fetchCharacter() {
   }
 }
 
-fetchCharacter();
+fetchCharacter(fetchurl);
